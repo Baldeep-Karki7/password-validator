5dc7f07f3c1636c31b0a05d3b678ba6038160c47
raphael <baldeepkarki77@gmail.com>
2024-02-15 17:30:24 +0545

    implementations added

--- validate.cpp
+++ validate.cpp
@@ -1,42 +1,110 @@
 #include <iostream>
 
-bool validate(std::string input)
+bool checkUcase(std::string pass)
 {
-    if (input.length() < 6)
+    for (char c : pass)
     {
+        if (isupper(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkLcase(std::string pass)
+{
+    for (char c : pass)
+    {
+        if (islower(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkDigit(std::string pass)
+{
+    for (char c : pass)
+    {
+        if (isdigit(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkSpecialChar(std::string pass)
+{
+    for (char c : pass)
+    {
+        if (!(isalnum(c)))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool validate(std::string pass)
+{
+    if (pass.length() < 7)
+    {
+        std::cout << "Length of password must be equal to or greater than 7"
+                  << "\n"
+                  << std::endl;
         return false;
     }
 
     bool hasUcase = false;
     bool hasLcase = false;
-    bool hasSpecialchar = false;
     bool hasdigit = false;
+    bool hasSpecialChar = false;
 
-    for (char c : input)
+    hasUcase = checkUcase(pass);
+    hasLcase = checkLcase(pass);
+    hasdigit = checkDigit(pass);
+    hasSpecialChar = checkSpecialChar(pass);
+
+    if (hasUcase == false)
     {
-        if (isupper(c))
-        {
-            hasUcase = true;
-        }
-        else if (islower(c))
-        {
-            hasLcase = true;
-        }
-        else if (isdigit(c))
-        {
-            hasdigit = true;
-        }
-        else if (!(isalnum(c)))
-        {
-            hasSpecialchar = true;
-        }
-
-        if (hasUcase && hasLcase && hasdigit && hasSpecialchar)
-        {
-            break;
-        }
+        std::cout << "At least one Upper Case letter required"
+                  << "\n"
+                  << std::endl;
     }
-    return true;
+    if (hasdigit == false)
+    {
+        std::cout << "At least one number required"
+                  << "\n"
+                  << std::endl;
+    }
+    if (hasSpecialChar == false)
+    {
+        std::cout << "At least one Special character required eg: ., @, # etc." << std::endl;
+    }
+
+    if (hasdigit && hasUcase && hasLcase && hasSpecialChar)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void display(bool result)
+{
+    if (result)
+    {
+        std::cout << "Your password is strong" << std::endl;
+    }
+    else
+    {
+        std::cout << "Your password is not strong enough" << std::endl;
+    }
 }
 
 int main()
@@ -45,12 +113,6 @@ int main()
     std::cout << "Enter your password" << std::endl;
     std::cin >> password;
 
-    if (validate(password))
-    {
-        std::cout << "Your password is strong" << std::endl;
-    }
-    else
-    {
-        std::cout << "Your password is not strong enough" << std::endl;
-    }
+    bool result = validate(password);
+    display(result);
 }
